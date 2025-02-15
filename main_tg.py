78600b6558003b21e24e2742e2a6b9f76c166b82
nogesh7 <122226852+nogesh7@users.noreply.github.com>
2025-02-15 17:36:26 +0300

    main_tg.py

--- main_tg.py
+++ main_tg.py
@@ -6,7 +6,6 @@ import pandas as pd
 #from docx import Document  # Correct usage of python-docx
 from spire.doc import *
 from spire.doc.common import *
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import re
 from datetime import datetime, date, time
 
@@ -20,7 +19,6 @@ with open("json_user.json", "r") as fh:
 with open("user_changed.json", "r") as fh:
     user_change = json.load(fh)
 
-print(user)
 
 global date
 global TIME
@@ -46,11 +44,13 @@ def start(message):
 
 @bot.message_handler(commands=['info'])
 def start(message):
-    if user[str(message.from_user.id)][1] == 4 and user[str(message.from_user.id)][0] == "adm":
+    if user[str(message.from_user.id)][0] == "adm":
         bot.send_message(message.from_user.id, "Напишите дату, на которую сейчас пришлете таблицу в формате ДД.ММ")
         user[str(message.from_user.id)][1] = "Присылает дату"
         with open("json_user.json", "w") as fh:
             json.dump(user, fh)
+    else:
+        bot.send_message(message.from_user.id, "Вы не админ")
 
 
 @bot.message_handler(commands=['send_time'])
@@ -63,9 +63,8 @@ def start(message):
 def get_text_messages(message):
     with open("json_user.json", "r") as fh:
         user = json.load(fh)
-    print(user)
     if user[str(message.from_user.id)][1] == 0:
-        bot.send_message(message.from_user.id, "Подтвердите что ваша фамилия:" + message.text)
+        bot.send_message(message.from_user.id, "Подтвердите что ваша фамилия:" + message.text + " (Введите свою фамилию еще раз)")
         user[str(message.from_user.id)][1] = 1
         user[str(message.from_user.id)][2] = message.text
         user_change[str(message.from_user.id)][0][3] = message.text
@@ -84,22 +83,24 @@ def get_text_messages(message):
 
         elif message.text != user[str(message.from_user.id)][2]:
             user[str(message.from_user.id)][1] = 0
-            print(1)
+
 
         with open("json_user.json", "w") as fh:
             json.dump(user, fh)
 
     elif user[str(message.from_user.id)][1] == 2:## проверка статуса админа
+        print(config.admin)
         if message.text == config.admin:
             user[str(message.from_user.id)][0] = "adm" ## выдача статуса админа или обычного юзера
         else:
             user[str(message.from_user.id)][0] = "user"
-        print(user)
+
         user[str(message.from_user.id)][1] = 3
         
         if user[str(message.from_user.id)][0] == "adm":
             bot.send_message(message.from_user.id, "Пришлите список фамилий (Каждая фамилия с новой строки)") ## у пользователя статус 3
-        
+        else:
+            bot.send_message(message.from_user.id, "Ключ неправильный(")
         with open("json_user.json", "w") as fh:
             json.dump(user, fh)
 
@@ -112,8 +113,7 @@ def get_text_messages(message):
             spisoc_polza.append(i[2]) ## b - список имен согласно тому, какие пользователи заходили
         spisoc_polz = sorted(spisoc_polza)
         spisoc_polz_ot_admina = sorted(message.text.split(sep='\n')) ## список имён от админа
-        print(spisoc_polz)
-        print(spisoc_polz_ot_admina)
+
 
 
         if spisoc_polz in spisoc_polz_ot_admina and len(spisoc_polz) < len(spisoc_polz_ot_admina):
@@ -126,7 +126,7 @@ def get_text_messages(message):
             bot.send_message(message.from_user.id, "ФИО незарегистрированых пользователей:" + q)
 
         elif spisoc_polz == spisoc_polz_ot_admina:
-            print(spisoc_polz)
+
             bot.send_message(message.from_user.id, "Все работники зарегистрированы")
 
         elif spisoc_polz_ot_admina in spisoc_polz and len(spisoc_polz) > len(spisoc_polz_ot_admina):
@@ -175,16 +175,16 @@ def get_document_messages(message):
             for j in range(0, tables.Count): ## перебор таблиц
                 table = tables.get_Item(j)
                 for m in range(0, table.Rows.Count):
-                    info = []
+                    info = ""
                     for n in range(0, table.Rows.get_Item(m).Cells.Count): # перебор таблицы
 
                         cell = table.Rows.get_Item(m).Cells.get_Item(n)
                         cellText = ""
                         if m != 0: # вся остальная информация о съемке
+
                             for para in range(cell.Paragraphs.Count):
                                 paragraphText = cell.Paragraphs.get_Item(para).Text
-                                info.append(paragraphText)
-
+                                info += "\n" + paragraphText
                             if n == 3: # добавление времени и даты 
                                 cor = table.Rows.get_Item(m).Cells.get_Item(n + 1)
                                 corr = ""
@@ -196,7 +196,7 @@ def get_document_messages(message):
                                 for para in range(t.Paragraphs.Count):
                                     time.append(t.Paragraphs.get_Item(para).Text) # текст ячейки с временем
                                     Time = ' '.join(time)
-                                print(Time)
+
 
                                 a = []
                                 for w in range(len(Time) - 2):
@@ -216,7 +216,7 @@ def get_document_messages(message):
                                             value[i][0] = date
                                             value[i][1] = TIME
                                             value[i][2] = info
-                                
+                        
         with open("json_user.json", "w") as fh:
             json.dump(user, fh)
 
@@ -225,8 +225,8 @@ def get_document_messages(message):
 
 
         for Id, value in user_change.items():
-            print(Id)
-            if datetime.now().timetuple().tm_mon >= int(mounth) and datetime.now().timetuple().tm_mday >= int(Date) and datetime.now().timetuple().tm_hour + 3 >= int(time_rass) and user_change[str(Id)][0][1] != "00:01":
+            print(user_change[str(Id)][0][2])
+            if user_change[str(Id)][0][1] != "00:01":
                 bot.send_message(int(Id), user_change[str(Id)][0][2])
 
        
@@ -239,21 +239,21 @@ def time_min():
     times = []
     for key, value in user_change.items():
         times.append(int(re.split(r'[;,.:\s]+', value[0][1])[0])) ##  Достаем значения часов
-        print(times)
     time_rass = min(times)
-
-if user_change:
-    for Id, value in user_change.items():
-        mounth, Date, time = re.split(r'[;,.:\s]+', user_change[str(Id)][0][0])[1], re.split(r'[;,.:\s]+', user_change[str(Id)][0][0])[0], re.split(r'[;,.:\s]+', user_change[str(Id)][0][0])[1]
-        time_min()
-        date = value[0][0]
-        if time_rass >= 9:
-            time_rass -= 24
-        if datetime.now().timetuple().tm_mon >= int(mounth) and datetime.now().timetuple().tm_mday >= int(Date) and int(time_rass) + 15 <= datetime.now().timetuple().tm_hour and user_change[str(Id)][0][1] != "00:01": ## проверка на час и день
+try:
+    if user_change:
+        for Id, value in user_change.items():
+            mounth, Date, time = re.split(r'[; ,.:\s]+', user_change[str(Id)][0][0])[1], re.split(r'[; ,.:\s]+', user_change[str(Id)][0][0])[0], re.split(r'[;,.:\s]+', user_change[str(Id)][0][0])[1]
+            time_min()
+            date = value[0][0]
+            if time_rass >= 9:
+                time_rass -= 24
+            if datetime.now().timetuple().tm_mon >= int(mounth) and datetime.now().timetuple().tm_mday >= int(Date) and int(time_rass) + 15 <= datetime.now().timetuple().tm_hour and user_change[str(Id)][0][1] != "00:01": ## проверка на час и день
                 bot.send_message(int(Id), user_change[str(Id)][0][2]) ## рассылка сообщений
                 value.append(["01.01", "00:01", "остальная информация", "Имя фамилия"])
                 user_change[str(Id)].pop(0)
-
+except:
+    bot.send_message(int(Id), "Дата в неверном формате")
 
     with open("json_user.json", "w") as fh:
             json.dump(user, fh)    
